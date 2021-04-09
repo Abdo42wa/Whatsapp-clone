870a10788f690d8833b1ca8a2f498abf7c6f0702
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-04-09 22:00:30 +0300

    crating the sidebare component and installing material-ui

---
+++ src/components/Sidebar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            <div className='sidebar-header'>
+
+            </div>
+            <div className='sidebar-search'>
+                
+            </div>
+
+            <div className='sidebar-chats'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCuHMxRunsBWiCD67fFFJtMKn618KUSwd0",
+    authDomain: "whatsapp-clone-70fdb.firebaseapp.com",
+    projectId: "whatsapp-clone-70fdb",
+    storageBucket: "whatsapp-clone-70fdb.appspot.com",
+    messagingSenderId: "1022098703033",
+    appId: "1:1022098703033:web:1985c63c2b8b66dc4fd168",
+    measurementId: "G-2X15HC06BZ"
+  };
