58ac748421da1668214b70e45d74d6508eb9fd7d
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-04-10 13:19:19 +0300

    finishing sidebar component and also adding a search box style

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,27 @@
 import React from 'react'
-
+import {Avatar,IconButton} from '@material-ui/core'
+import { MoreVert, Chat, DonutLarge, SearchOutlined } from '@material-ui/icons';
 const Sidebar = () => {
     return (
         <div className='sidebar'>
             <div className='sidebar-header'>
-
+                <Avatar />
+                <div className='sidebar-headerRight'>
+                <IconButton >
+                <DonutLarge />
+                </IconButton>
+                   <IconButton><Chat /></IconButton>
+                   <IconButton><MoreVert /></IconButton>
+                   
+                   
+                </div>
             </div>
             <div className='sidebar-search'>
-                
+            <div className='sidebar-searchContainer'>
+                <SearchOutlined />
+                <input placeholder="search or start new chat " type='text' />
+            </div>
+               
             </div>
 
             <div className='sidebar-chats'>
